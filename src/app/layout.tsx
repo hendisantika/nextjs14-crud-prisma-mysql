812ee533f6cbbdb9ba892238e8b66340b7791f0e
hendisantika <hendisantika@yahoo.co.id>
2024-07-25 07:02:33 +0700

    Modify layout.tsx page 🫸🌀✏️📗🐧🐳

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {Toaster} from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,13 +11,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                       children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={inter.className}>
+      <main>{children}</main>
+      <Toaster/>
+      </body>
+      </html>
   );
 }
